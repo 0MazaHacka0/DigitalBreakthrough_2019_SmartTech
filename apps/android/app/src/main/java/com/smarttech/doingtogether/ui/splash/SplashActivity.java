d5c1ce9c2cbfde0a2737b5a66e7d26e84fbdb3cb
Dmitry Abakumov <killerinshadow2@gmail.com>
2019-06-08 17:18:09 +0300

    Main screen layout

---
+++ apps/android/app/src/main/java/com/smarttech/doingtogether/ui/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.smarttech.doingtogether.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smarttech.doingtogether.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
