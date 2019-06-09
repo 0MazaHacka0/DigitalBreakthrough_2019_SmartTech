6a89eb9dd9205c16aadff7b250bdf403018087da
Dmitry Abakumov <killerinshadow2@gmail.com>
2019-06-09 15:38:26 +0300

    Event screen

---
+++ apps/android/app/src/main/java/com/smarttech/doingtogether/ui/event/EventActivity.java
@@ -0,0 +1,30 @@
+package com.smarttech.doingtogether.ui.event;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.smarttech.doingtogether.R;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.select_ride_layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EventActivity.this, SelectActivity.class));
+            }
+        });
+    }
+
+}
