f0bcf889136673dd7a18e22fdc8d713adb4dcd6b
Dmitry Abakumov <killerinshadow2@gmail.com>
2019-06-09 03:40:15 +0300

    Api

--- apps/android/app/src/main/java/com/smarttech/doingtogether/data/network/ApiHelper.java
+++ apps/android/app/src/main/java/com/smarttech/doingtogether/data/network/ApiHelper.java
@@ -1,5 +1,21 @@
 package com.smarttech.doingtogether.data.network;
 
+import com.smarttech.doingtogether.data.network.model.DomainResponse;
+import com.smarttech.doingtogether.data.network.model.LoginRequest;
+import com.smarttech.doingtogether.data.network.model.LoginResponse;
+
+import io.reactivex.Observable;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface ApiHelper {
+
+    @POST("login")
+    Observable<LoginResponse> login(@Body LoginRequest request);
+
+    @GET("domain")
+    Observable<DomainResponse> getIconByDomain(@Query("domain") String domain);
 
 }
--- apps/android/app/src/main/java/com/smarttech/doingtogether/utils/AppConstants.java
+++ apps/android/app/src/main/java/com/smarttech/doingtogether/utils/AppConstants.java
@@ -4,7 +4,7 @@ public class AppConstants {
 
     // API
     // public static final String API_AUTHORIZATION_HEADER = null;
-    public static final String API_BASE_URL = "http://2537ly.space:8345/api/";
+    public static final String API_BASE_URL = "https://test-discord.ga/api/";
 
     // public static final String API_PASSWORD_AUTHENTICATION = "password";
     // public static final String API_TOKEN_AUTHENTICATION = "refresh_token";
